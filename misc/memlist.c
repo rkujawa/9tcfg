278a7e6ae28a7faf15670041842d3def1e5999ef
Radosław Kujawa <radoslaw.kujawa@c0ff33.net>
2013-01-19 18:56:28 +0100

    Add small utility to list memory regions.

---
+++ misc/memlist.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+#include <exec/types.h>
+#include <exec/execbase.h>
+
+#include <proto/exec.h>
+
+int
+main(void)
+{
+	struct MemHeader *m, *nm;
+
+	for (m  = (void *) SysBase->MemList.lh_Head;
+	    nm = (void *) m->mh_Node.ln_Succ; m = nm) {
+		printf("%p to %p", (void*) m->mh_Lower, (void*) m->mh_Upper);
+	}
+
+	return 0;
+}
+
